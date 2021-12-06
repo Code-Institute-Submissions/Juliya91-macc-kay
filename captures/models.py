34e74acc5635e826f5893e5afbaa989c5ce36fd3
Juliya91 <juliyarezanovich@gmail.com>
2021-12-06 17:31:13 +0000

    Add artist and captures models

--- captures/models.py
+++ captures/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    class Meta:
+        verbose_name_plural = 'Artists'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    artist_statement = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField()
+    instagram_url = models.URLField(max_length=1024, null=True, blank=True)
+    website_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Captures(models.Model):
+    artist = models.ForeignKey(
+        'Artist', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
