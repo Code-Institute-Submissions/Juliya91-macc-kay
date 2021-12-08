bfb2d62d49fc0b6aa4dc9adce311bb58bcc5f084
Juliya91 <juliyarezanovich@gmail.com>
2021-12-08 15:07:48 +0000

    Customize admin.py - Add CapturesAdmin & ArtistAdmin classes

--- captures/admin.py
+++ captures/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from .models import Captures, Artist
 
-admin.site.register(Captures)
-admin.site.register(Artist)
+
+class CapturesAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'artist',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'artist_statement',
+    )
+
+
+admin.site.register(Captures, CapturesAdmin)
+admin.site.register(Artist, ArtistAdmin)
