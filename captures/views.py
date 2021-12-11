ceca5804ac6fdaa10cd531f6d3930860aeb5b561
Juliya91 <juliyarezanovich@gmail.com>
2021-12-11 12:02:39 +0000

    Add search functionallity

--- captures/views.py
+++ captures/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Captures
 
 
 def all_captures(request):
     """ A view to show all captures, including sorting and search queries """
     captures = Captures.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('captures'))
+
+            queries = Q(
+                name__icontains=query) | Q(description__icontains=query)
+            captures = captures.filter(queries)
 
     context = {
         'captures': captures,
+        'search_term': query,
     }
 
     return render(request, 'captures/captures.html', context)
