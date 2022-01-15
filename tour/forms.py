3bbc958a446abad13ff56d5261f4fe4d95100b2c
Juliya91 <juliyarezanovich@gmail.com>
2022-01-15 17:40:38 +0000

    Add book a tour form

--- tour/forms.py
+++ tour/forms.py
@@ -1,9 +1,17 @@
 from django import forms
 from .models import Tour
+from django.forms import ModelForm
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 
 class TourForm(forms.ModelForm):
     class Meta:
         model = Tour
         fields = ('first_name', 'last_name',
-                  'email', 'preferred_time',)
+                  'email', 'preferred_time', 'preferred_date',)
+        widgets = {
+            'preferred_date': DateInput(),
+        }
--- tour/models.py
+++ tour/models.py
@@ -5,4 +5,14 @@ class Tour(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, null=False, blank=False)
-    preferred_time = models.CharField(max_length=100)
+    preferred_date = models.DateField(null=True)
+
+    PREFERRED_TIME_CHOICES = [
+        ('13:30', '13:30'),
+        ('15:30', '15:30'),
+    ]
+
+    preferred_time = models.CharField(
+        max_length=5,
+        choices=PREFERRED_TIME_CHOICES,
+    )
