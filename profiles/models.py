d53e339095907273563513bc46fe2ee65fa20df3
Juliya91 <juliyarezanovich@gmail.com>
2022-01-16 14:28:19 +0000

    Change create or update user profile function

--- profiles/models.py
+++ profiles/models.py
@@ -36,7 +36,5 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     Create or update the user profile
     """
-    if created:
-        UserProfile.objects.create(user=instance)
-    # Existing users: just save the profile
-    instance.userprofile.save()
+    profile = UserProfile.objects.get_or_create(user=instance)
+    profile.save()
