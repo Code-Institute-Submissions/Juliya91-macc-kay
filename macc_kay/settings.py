c1293c9047744e06963310591cccaed33c71038b
Juliya91 <juliyarezanovich@gmail.com>
2022-01-16 01:21:24 +0000

    Add profiles app

--- macc_kay/settings.py
+++ macc_kay/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'cart',
     'checkout',
     'tour',
+    'profiles',
 
     # Other
     'crispy_forms',
--- macc_kay/urls.py
+++ macc_kay/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('captures/', include('captures.urls')),
+    path('tour/', include('tour.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
-    path('tour/', include('tour.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
